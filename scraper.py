47539c3f1a07cb64c2b809ff6a1d1c5c39371961
orangecola <orangecola3@gmail.com>
2019-02-08 17:05:49 +0800

    Adding initial scraper

---
+++ scraper.py
@@ -0,0 +1,80 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import sys, json, urllib
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+f = '{0}:\t{1}'	
+print('https://tcgplayer.com')
+print()
+
+with urllib.request.urlopen('https://api.scryfall.com/cards/search?q=' + urllib.parse.quote(sys.argv[1])) as url:
+    data = json.loads(url.read().decode())
+for i in data['data']:
+	print(i['name'])
+	print(i['set_name'])
+	print(f.format('USD', i['usd']))
+
+print()
+print('https://mtg.bigweb.co.jp')
+print()
+raw_html = simple_get('https://mtg.bigweb.co.jp/cards/filter?big_keyword=' + sys.argv[1])
+html = BeautifulSoup(raw_html, 'html.parser')
+root = html.find("div", class_="row imageview")
+children = root.findChildren(recursive=False)
+for i in range(0, len(children)):
+	if 'item' in children[i]['class']:
+		#Display Card Name
+		parsed_json = json.loads(children[i].findChildren(recursive=False)[0]['data-obj'])
+		print(parsed_json['name'])
+		try:
+			#Display Card Prices
+			pricelist = children[i].findChildren(recursive=False)[0].findChildren(recursive=False)[3].findChildren(recursive=False)[2].findChildren(recursive=False)[0].findChildren(recursive=False)
+		except:
+			#Sold Out
+			print(f.format('Sold out', '0円'))
+			continue
+		pricelist = pricelist[:-1]
+		for i in pricelist:
+			condition = i.findChildren(recursive=False)[0].findChildren(recursive=False)[0].text 
+			price = i.findChildren(recursive=False)[0].findChildren(recursive=False)[1].text
+			print(f.format(condition, price))
+	else:
+		#Display Set Name
+		print (children[i].findChildren()[0].findChildren()[1].findChildren()[0].text)
