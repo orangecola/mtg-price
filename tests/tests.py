693c3ac5e67d131edefe9f6140a74abe6ba66aba
Joel Teo <j.teo@sec-consult.com>
2019-12-19 17:29:57 +0800

    Added beginnings of test suite

---
+++ tests/tests.py
@@ -0,0 +1,18 @@
+import unittest
+import config
+import os
+class Test_Bigweb(unittest.TestCase):
+    def test_withoutCache(self):
+        pass
+
+    def test_withCache(self):
+        pass
+
+    def test_nameTransformation(self):
+        self.assertEqual(1, 1)
+    
+    def test_errorHandling(self):
+        pass
+
+    def test_Combination(self):
+        pass
